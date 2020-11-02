3a211b82e404926074e8c5a3d7cbac8d56cdeb12
MuhammadArifFaizin <muhammadariffaizin@gmail.com>
2020-11-03 00:43:59 +0700

    Carousel upgrade

--- src/components/CarouselBanner.js
+++ src/components/CarouselBanner.js
@@ -8,9 +8,10 @@ import {
   CardContent,
   CardMedia,
   Typography,
-  Grid,
   Button,
 } from '@material-ui/core';
+
+import carouselItems from '../data/carouselItems';
 
 const useStyles = makeStyles((theme) => ({
   Banner: {
@@ -20,52 +21,35 @@ const useStyles = makeStyles((theme) => ({
     '& $Media': {
       backgroundColor: theme.palette.common.white,
       height: '100%',
+      width: '100%',
       overflow: 'hidden',
-      position: 'relative',
+      position: 'absolute',
       transition: '300ms',
       cursor: 'pointer',
       '&:hover': {
         filter: 'brightness(115%)',
       },
-
-      '& $MediaCaption': {
-        textOverflow: 'ellipsis',
-        position: 'absolute',
-        bottom: '0',
-        padding: theme.spacing(2),
-        backgroundColor: theme.palette.common.black,
-        color: theme.palette.common.white,
-        opacity: '0.6',
-        width: '100%',
-        fontWeight: theme.typography.fontWeightLight,
-        transition: '300ms',
-        cursor: 'pointer',
-        '&:hover': {
-          opacity: '0.8',
-        },
-      },
-    },
-
-    '& $BannerGrid': {
-      height: '100%',
     },
 
     '& $Content': {
       color: theme.palette.common.white,
-      backgroundColor: theme.palette.primary.main,
+      backgroundImage:
+        'linear-gradient(to right, rgba(0,0,0, 0.9) , transparent);',
       height: '100%',
+      width: '100%',
       cursor: 'pointer',
-      paddingTop: theme.spacing(10),
+      position: 'absolute',
+      paddingTop: theme.spacing(12),
       paddingBottom: theme.spacing(4),
       paddingLeft: theme.spacing(4),
       paddingRight: theme.spacing(4),
       transition: theme.transitions.duration.standard + 'ms',
       '&:hover, &:active': {
-        backgroundColor: theme.palette.primary.main,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
 
         '& $ViewButton': {
           backgroundColor: theme.palette.common.white,
-          color: theme.palette.primary.main,
+          color: theme.palette.common.black,
         },
       },
 
@@ -97,8 +81,6 @@ const useStyles = makeStyles((theme) => ({
     },
   },
   Media: {},
-  MediaCaption: {},
-  BannerGrid: {},
   Content: {},
   ViewButton: {},
   Title: {},
@@ -107,122 +89,46 @@ const useStyles = makeStyles((theme) => ({
 
 function Banner(props) {
   if (props.newProp) console.log(props.newProp);
-  const contentPosition = props.contentPosition
-    ? props.contentPosition
-    : 'left';
-  const totalItems = props.length ? props.length : 3;
-  const mediaLength = totalItems - 1;
 
   const classes = useStyles();
 
   let items = [];
   const content = (
-    <Grid item xs={12 / totalItems} key='content'>
-      <CardContent className={classes.Content}>
-        <Typography variant='h4' className={classes.Title}>
-          {props.item.Name}
-        </Typography>
+    <CardContent className={classes.Content} key='content'>
+      <Typography variant='h4' className={classes.Title}>
+        {props.item.Name}
+      </Typography>
 
-        <Typography className={classes.Caption}>
-          {props.item.Caption}
-        </Typography>
+      <Typography className={classes.Caption}>{props.item.Caption}</Typography>
 
-        <Button variant='outlined' className={classes.ViewButton}>
-          View Now
-        </Button>
-      </CardContent>
-    </Grid>
+      <Button variant='outlined' className={classes.ViewButton}>
+        View Now
+      </Button>
+    </CardContent>
   );
 
-  for (let i = 0; i < mediaLength; i++) {
-    const item = props.item.Items[i];
+  const media = (
+    <CardMedia
+      className={classes.Media}
+      image={props.item.Image}
+      title={props.item.Name}
+      key={props.item.Name}
+    ></CardMedia>
+  );
 
-    const media = (
-      <Grid item xs={12 / totalItems} key={item.Name}>
-        <CardMedia
-          className={classes.Media}
-          image={item.Image}
-          title={item.Name}
-        >
-          <Typography className={classes.MediaCaption}>{item.Name}</Typography>
-        </CardMedia>
-      </Grid>
-    );
-
-    items.push(media);
-  }
-
-  if (contentPosition === 'left') {
-    items.unshift(content);
-  } else if (contentPosition === 'right') {
-    items.push(content);
-  } else if (contentPosition === 'middle') {
-    items.splice(items.length / 2, 0, content);
-  }
+  items.push(media);
+  items.push(content);
 
   return (
     <Card raised className={classes.Banner}>
-      <Grid container spacing={0} className={classes.BannerGrid}>
-        {items}
-      </Grid>
+      {items}
     </Card>
   );
 }
 
-const items = [
-  {
-    Name: 'Bali Kulturalis',
-    Caption: 'Nikmati pertunjukan penuh dengan khazanah budaya berkesan!',
-    contentPosition: 'left',
-    Items: [
-      {
-        Name: 'Tari Barong',
-        Image:
-          'https://images.unsplash.com/photo-1565970141926-c001afaf8577?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-      },
-      {
-        Name: 'Tari Kecak',
-        Image:
-          'https://images.unsplash.com/photo-1511164657592-59a452023479?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
-      },
-    ],
-  },
-  {
-    Name: 'Pesona Jawa',
-    Caption: 'Jelajahi pesona dari jawa dengan pertunjukan menakjubkan!',
-    contentPosition: 'middle',
-    Items: [
-      {
-        Name: 'Tari Topeng',
-        Image:
-          'https://images.unsplash.com/photo-1508684602137-2fe4e5539fa3?ixlib=rb-1.2.1&auto=format&fit=crop&w=1037&q=80',
-      },
-      {
-        Name: 'Tari Srigati',
-        Image:
-          'https://images.unsplash.com/photo-1599551634107-95ce4e763232?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
-      },
-    ],
-  },
-  {
-    Name: 'Decoratives',
-    Caption: 'Give style and color to your living room!',
-    contentPosition: 'right',
-    Items: [
-      {
-        Name: 'Living Room Lamp',
-        Image: 'https://source.unsplash.com/featured/?lamp',
-      },
-      {
-        Name: 'Floral Vase',
-        Image: 'https://source.unsplash.com/featured/?vase',
-      },
-    ],
-  },
-];
-
 function CarouselBanner() {
   const classes = useStyles();
+  const items = carouselItems;
   return (
     <React.Fragment>
       <Carousel
@@ -236,13 +142,7 @@ function CarouselBanner() {
         navButtonsAlwaysInVisible={false}
       >
         {items.map((item, index) => {
-          return (
-            <Banner
-              item={item}
-              key={index}
-              contentPosition={item.contentPosition}
-            />
-          );
+          return <Banner item={item} key={index} />;
         })}
       </Carousel>
     </React.Fragment>
---
+++ src/data/carouselItems.js
@@ -0,0 +1,28 @@
+const carouselItems = [
+  {
+    Name: 'Tari Barong',
+    Caption: 'Nikmati pertunjukan penuh dengan khazanah budaya berkesan!',
+    Image:
+      'https://images.unsplash.com/photo-1565970141926-c001afaf8577?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+  },
+  {
+    Name: 'Tari Kecak',
+    Caption: 'Jelajahi pesona dari jawa dengan pertunjukan menakjubkan!',
+    Image:
+      'https://images.unsplash.com/photo-1511164657592-59a452023479?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80',
+  },
+  {
+    Name: 'Tari Topeng',
+    Caption: 'Memaknai setiap gerakan dengan budaya kearifan!',
+    Image:
+      'https://images.unsplash.com/photo-1508684602137-2fe4e5539fa3?ixlib=rb-1.2.1&auto=format&fit=crop&w=1037&q=80',
+  },
+  {
+    Name: 'Tari Srigati',
+    Caption: 'Gerakan mendalam penuh dengan makna!',
+    Image:
+      'https://images.unsplash.com/photo-1599551634107-95ce4e763232?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=334&q=80',
+  },
+];
+
+export default carouselItems;
